f7e6e6b7e3eed8bffc7de179611902c02dfe5b72
mnkcoders <mnkcoders@gmail.com>
2022-10-19 19:08:17 +0200

    Admin Startup integrations

--- components/controllers/admin.php
+++ components/controllers/admin.php
@@ -11,7 +11,27 @@ final class AdminController extends \CODERS\Framework\Request{
         
         parent::__construct($endpoint, $route );
     }
-    
+    /**
+     * @param array $args
+     * @return boolean
+     */
+    protected final function default_action(array $args = array()) {
+        
+        $view = $this->importView('admin');
+        if( !is_null($view)){
+            $view->setModel($this->importModel('item'))
+                    ->setDisplay('default')
+                    ->show();
+            
+            return true;
+        }
+        else{
+            $args['error'] = __('Invalid view [admin]','digital_creator');
+        }
+        return $this->error_action( $args );
+    }
+
+
     protected final function error_action( array $args = array()){
         var_dump($this->list());
         var_dump($this->action(TRUE));
