1ab369ddbaad2981004318bc3575ecc2276cd1de
Coder01 <mnkcoders@gmail.com>
2022-10-22 11:12:38 +0200

    Database Installer and Uninstaller Test View action url generator test

--- components/controllers/admin-setup.php
+++ components/controllers/admin-setup.php
@@ -17,6 +17,43 @@ final class SetupAdminController extends \CODERS\Framework\Request{
      * @param array $args
      * @return boolean
      */
+    protected final function install_action( array $args = array()){
+        
+        //var_dump($this);
+        
+        $db = new \CODERS\Framework\Query($this->endPoint());
+        
+        $schema = array(
+            'items' => array(
+                'id' => array( 'type' => 'incremental','size'=>8,'index'=>true,'required'=>true),
+                'name' => array( 'type' => 'text','size'=>32,'required'=>true),
+                'type' => array( 'type' => 'text','size'=>24,'required'=>true,'default'=>'general'),
+                'date_created' => array( 'type' => 'date_time'),
+                'date_updated' => array( 'type' => 'date_time','default'=>'CURRENT_TIMESTAMP'),
+            ),
+        );
+        
+        $db->__install($schema);
+        
+        //
+        return true;
+        
+    }
+    protected final function uninstall_action( array $args = array()){
+        
+        $db = new \CODERS\Framework\Query($this->endPoint());
+        $schema = array(
+            'items'
+        );
+        $db->__uninstall($schema);
+        
+        return true;
+    }
+
+        /**
+     * @param array $args
+     * @return boolean
+     */
     protected final function default_action(array $args = array()) {
         
         $view = $this->importView('setup');
--- components/views/setup/html/setup.php
+++ components/views/setup/html/setup.php
@@ -1,3 +1,6 @@
 <?php defined('ABSPATH') or die; ?>
 
-<p>Setup View</p>
+<ul>
+    <li><a href="<?php print $this->admin_action_setup_install ?>" target="_blank">Install</a></li>
+    <li><a href="<?php print $this->admin_action_setup_uninstall ?>" target="_blank">Uninstall</a></li>
+</ul>
